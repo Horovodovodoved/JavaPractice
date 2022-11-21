da1bcb9081e1306ed65ed3710b7e01d74f69229b
Horovodovodoved <99143195+Horovodovodoved@users.noreply.github.com>
2022-11-21 18:01:08 +0300

    Update FilmCharacter.java

--- list/FilmCharacter.java
+++ list/FilmCharacter.java
@@ -2,31 +2,38 @@ package list;
 
 import java.util.Scanner;
 
-class FilmCharacter {
-  FilmCharacter next;
-  String name;
-  String description;
-  String temper;
-  boolean isMarried;
-  
-  FilmCharacter() {}
-  
-  FilmCharacter(Scanner scan) { readAttributes(scan); }
-  
-  FilmCharacter getNext() { return next; }
-  
-  void setNext(FilmCharacter filmCharacter) { next = filmCharacter; }
-  
-  @Override
-  public String toString() { return name + "\n" + description + "\n" + temper +
-      "\n" + (isMarried ? " Женат." : "Не женат."); }
-  
-  void readAttributes(Scanner scan) {
-    name = scan.nextLine();
-    description = scan.nextLine();
-    temper = scan.nextLine();
-    isMarried = scan.next().equalsIgnoreCase("женат");
-  }
-  
-  void printAttributes() { System.out.println(this); }
+class FilmCharacter implements SimpleLinkedListNode {
+    FilmCharacter next;
+    String name;
+    String description;
+    String temper;
+    boolean isMarried;
+
+    FilmCharacter(Scanner scan) { readAttributes(scan); }
+
+    @Override
+    public String getID() { return name; }
+
+    @Override
+    public FilmCharacter getNext() { return next; }
+
+    @Override
+    public void setNext(SimpleLinkedListNode filmCharacter) { next = (FilmCharacter) filmCharacter; }
+
+    @Override
+    public void setNext(Scanner scan) { setNext(new FilmCharacter(scan)); }
+
+    @Override
+    public String toString() { return name + "\n" + description + "\n" + temper +
+            "\n" + (isMarried ? " Женат." : "Не женат."); }
+
+    @Override
+    public void readAttributes(Scanner scan) {
+        name = scan.nextLine();
+        description = scan.nextLine();
+        temper = scan.nextLine();
+        isMarried = scan.next().equalsIgnoreCase("женат");
+    }
+    @Override
+    public void printAttributes() { System.out.println(this); }
 }
