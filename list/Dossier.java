5926f20673d7b95ae145592b7a346f943d4c7ea8
Horovodovodoved <99143195+Horovodovodoved@users.noreply.github.com>
2022-11-21 17:59:12 +0300

    Update Dossier.java

--- list/Dossier.java
+++ list/Dossier.java
@@ -5,81 +5,102 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-// TODO: 3 methods: addFirst(elem), addLast(elem), addAfter(idx, elem)
-public class Dossier implements SimpleLinkedList<FilmCharacter> {
-  FilmCharacter zeroNode;
-  
-  @Override
-  public String toString() {
-    StringBuilder str = new StringBuilder();
-    FilmCharacter currNode = zeroNode;
-    while (currNode.getNext() != null) {
-      currNode = currNode.getNext();
-      str.append(currNode.toString());
+// TODO: 4 methods: addFirst(elem), addLast(elem)
+// TodO: #22, #23, #24, List_2
+public class Dossier implements SimpleLinkedList<SimpleLinkedListNode> {
+    SimpleLinkedListNode zeroNode;
+
+    @Override
+    public SimpleLinkedListNode getById(String id) {
+        SimpleLinkedListNode currNode = zeroNode;
+        while (currNode.getNext() != null) {
+            currNode = currNode.getNext();
+            if (currNode.getID().equals(id)) {
+                return currNode;
+            }
+        }
+        return currNode;
     }
-    return str.toString();
-  }
-  
-  @Override
-  public void add(FilmCharacter filmCharacter) {
-    filmCharacter.setNext(zeroNode.getNext());
-    zeroNode.setNext(filmCharacter);
-  }
-  
-  @Override
-  public boolean remove(String name) {
-    FilmCharacter prevNode = zeroNode;
-    FilmCharacter currNode = zeroNode;
-    while (currNode.getNext() != null) {
-      currNode = currNode.getNext();
-      if (currNode.name.equals(name)) {
-        prevNode.setNext(currNode.getNext());
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        SimpleLinkedListNode currNode = zeroNode;
+        while (currNode.getNext() != null) {
+            currNode = currNode.getNext();
+            str.append(currNode.toString());
+        }
+        return str.toString();
+    }
+
+    @Override
+    public void add(SimpleLinkedListNode node) {
+        node.setNext(zeroNode.getNext());
+        zeroNode.setNext(node);
+    }
+
+    @Override
+    public boolean addAfter(SimpleLinkedListNode node_to_add, String previous_node_id) {
+        SimpleLinkedListNode previous_node = getById(previous_node_id);
+        if (previous_node == null) {
+            return false;
+        }
+        node_to_add.setNext(previous_node.getNext());
+        previous_node.setNext(node_to_add);
         return true;
-      }
-      prevNode = currNode;
     }
-    return false;
-  }
-  
-  @Override
-  public boolean printNode(String name) {
-    FilmCharacter currNode = zeroNode;
-    while (currNode.getNext() != null) {
-      currNode = currNode.getNext();
-      if (currNode.name.equals(name)) {
-        currNode.printAttributes();
-        return true;
-      }
+
+    @Override
+    public boolean remove(String id) {
+        SimpleLinkedListNode prevNode = zeroNode;
+        SimpleLinkedListNode currNode = zeroNode;
+        while (currNode.getNext() != null) {
+            currNode = currNode.getNext();
+            if (currNode.getID().equals(id)) {
+                prevNode.setNext(currNode.getNext());
+                return true;
+            }
+            prevNode = currNode;
+        }
+        return false;
     }
-    return false;
-  }
-  
-  @Override
-  public void clear() { zeroNode.setNext(null); }
-  
-  @Override
-  public boolean isEmpty() { return zeroNode.getNext() == null; }
-  
-  @Override
-  public void printToFile(String filename) {
-    try (FileWriter fw = new FileWriter(filename)) {
-      fw.write(toString());
-    } catch (IOException ex) {
-      System.out.println(ex.getMessage());
+
+    @Override
+    public boolean printNode(String id) {
+        SimpleLinkedListNode node = getById(id);
+        if (node != null) {
+            node.printAttributes();
+            return true;
+        }
+        return false;
     }
-  }
-  
-  @Override
-  public void readFromFile(String filename) {
-    try (FileReader fr = new FileReader(filename)) {
-      Scanner fileScan = new Scanner(fr);
-      FilmCharacter currNode = zeroNode;
-      while (fileScan.hasNextLine()) {
-        currNode.setNext(new FilmCharacter(fileScan));
-        currNode = currNode.getNext();
-      }
-    } catch (Exception ex) {
-      System.out.println(ex.getMessage());
+
+    @Override
+    public void clear() { zeroNode.setNext((SimpleLinkedListNode) null); }
+
+    @Override
+    public boolean isEmpty() { return zeroNode.getNext() == null; }
+
+    @Override
+    public void writeToFile(String filename) {
+        try (FileWriter fw = new FileWriter(filename)) {
+            fw.write(this.toString());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
-  }
+
+    @Override
+    public void readFromFile(String filename) {
+        try (FileReader fr = new FileReader(filename)) {
+            Scanner fileScan = new Scanner(fr);
+            SimpleLinkedListNode currNode = zeroNode;
+            while (fileScan.hasNextLine()) {
+                currNode.setNext(fileScan);
+                currNode = currNode.getNext();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
